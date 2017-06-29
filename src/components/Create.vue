16245d6df2aec209b12b257d242a4a7361dd1d4d
Stephen Ma <stephen@Stephens-MBP.fios-router.home>
2017-06-28 22:46:10 -0500

    three column form

--- src/components/Create.vue
+++ src/components/Create.vue
@@ -1,6 +1,6 @@
 <template>
   <temp>
-    <div class="column is-4 is-offset-4 has-text-right">
+    <div class="column is-4 is-offset-4">
       <div class="field is-horizontal">
         <div class="field-label is-normal">
           <label class="label">Teams</label>
@@ -20,344 +20,114 @@
           </div>
         </div>
       </div>
-      <!--<table>
-                  <tbody>
-                    <tr>
-                      <td>
-                        <div class="field is-horizontal">
-                          <div class="field-label is-normal">
-                            <label class="label">QB</label>
-                          </div>
-                          <div class="field-body">
-                            <div class="field">
-                              <p class="control">
-                                <span class="select">
-                                  <select>
-                                    <option value=""></option>
-                                    <option value=""></option>
-                                    <option value=""></option>
-                                    <option value=""></option>
-                                  </select>
-                                </span>
-                              </p>
-                            </div>
-                          </div>
-                        </div>
-                      </td>
-                      <td>
-                        <div class="field is-horizontal">
-                          <div class="field-label is-normal">
-                            <label class="label">Flex</label>
-                          </div>
-                          <div class="field-body">
-                            <div class="field">
-                              <p class="control">
-                                <span class="select">
-                                  <select>
-                                    <option value=""></option>
-                                    <option value=""></option>
-                                    <option value=""></option>
-                                    <option value=""></option>
-                                  </select>
-                                </span>
-                              </p>
-                            </div>
-                          </div>
-                        </div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <div class="field is-horizontal">
-                          <div class="field-label is-normal">
-                            <label class="label">RB</label>
-                          </div>
-                          <div class="field-body">
-                            <div class="field">
-                              <p class="control">
-                                <span class="select">
-                                  <select>
-                                    <option value=""></option>
-                                    <option value=""></option>
-                                    <option value=""></option>
-                                    <option value=""></option>
-                                  </select>
-                                </span>
-                              </p>
-                            </div>
-                          </div>
-                        </div>
-                      </td>
-                      <td>
-                        <div class="field is-horizontal">
-                          <div class="field-label is-normal">
-                            <label class="label">D/ST</label>
-                          </div>
-                          <div class="field-body">
-                            <div class="field">
-                              <p class="control">
-                                <span class="select">
-                                  <select>
-                                    <option value=""></option>
-                                    <option value=""></option>
-                                    <option value=""></option>
-                                    <option value=""></option>
-                                  </select>
-                                </span>
-                              </p>
-                            </div>
-                          </div>
-                        </div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <div class="field is-horizontal">
-                          <div class="field-label is-normal">
-                            <label class="label">WR</label>
-                          </div>
-                          <div class="field-body">
-                            <div class="field">
-                              <p class="control">
-                                <span class="select">
-                                  <select>
-                                    <option value=""></option>
-                                    <option value=""></option>
-                                    <option value=""></option>
-                                    <option value=""></option>
-                                  </select>
-                                </span>
-                              </p>
-                            </div>
-                          </div>
-                        </div>
-                      </td>
-                      <td>
-                        <div class="field is-horizontal">
-                          <div class="field-label is-normal">
-                            <label class="label">K</label>
-                          </div>
-                          <div class="field-body">
-                            <div class="field">
-                              <p class="control">
-                                <span class="select">
-                                  <select>
-                                    <option value=""></option>
-                                    <option value=""></option>
-                                    <option value=""></option>
-                                    <option value=""></option>
-                                  </select>
-                                </span>
-                              </p>
-                            </div>
-                          </div>
-                        </div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <div class="field is-horizontal">
-                          <div class="field-label is-normal">
-                            <label class="label">TE</label>
-                          </div>
-                          <div class="field-body">
-                            <div class="field">
-                              <p class="control">
-                                <span class="select">
-                                  <select>
-                                    <option value=""></option>
-                                    <option value=""></option>
-                                    <option value=""></option>
-                                    <option value=""></option>
-                                  </select>
-                                </span>
-                              </p>
-                            </div>
-                          </div>
-                        </div>
-                      </td>
-                      <td>
-                        <div class="field is-horizontal">
-                          <div class="field-label is-normal">
-                            <label class="label">Bench</label>
-                          </div>
-                          <div class="field-body">
-                            <div class="field">
-                              <p class="control">
-                                <span class="select">
-                                  <select>
-                                    <option value=""></option>
-                                    <option value=""></option>
-                                    <option value=""></option>
-                                    <option value=""></option>
-                                  </select>
-                                </span>
-                              </p>
-                            </div>
-                          </div>
-                        </div>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>-->
       <div class="columns">
-        <div class="column has-text-right is-narrow">
-          <div class="field is-horizontal">
-            <div class="field-label is-normal">
-              <label class="label">QB</label>
-            </div>
-            <div class="field-body">
-              <div class="field">
-                <p class="control">
-                  <span class="select">
-                    <select>
-                      <option value=""></option>
-                      <option value=""></option>
-                      <option value=""></option>
-                      <option value=""></option>
-                    </select>
-                  </span>
-                </p>
-              </div>
-            </div>
+        <div class="column">
+          <div class="field">
+            <label class="label">QB</label>
+            <p class="control">
+              <span class="select">
+                <select>
+                  <option></option>
+                  <option></option>
+                </select>
+              </span>
+            </p>
           </div>
-          <div class="field is-horizontal">
-            <div class="field-label is-normal">
-              <label class="label">RB</label>
-            </div>
-            <div class="field-body">
-              <div class="field">
-                <p class="control">
-                  <span class="select">
-                    <select>
-                      <option value=""></option>
-                      <option value=""></option>
-                      <option value=""></option>
-                      <option value=""></option>
-                    </select>
-                  </span>
-                </p>
-              </div>
-            </div>
+          <div class="field">
+            <label class="label">RB</label>
+            <p class="control">
+              <span class="select">
+                <select>
+                  <option></option>
+                  <option></option>
+                </select>
+              </span>
+            </p>
           </div>
-          <div class="field is-horizontal">
-            <div class="field-label is-normal">
-              <label class="label">WR</label>
-            </div>
-            <div class="field-body">
-              <div class="field">
-                <p class="control">
-                  <span class="select">
-                    <select>
-                      <option value=""></option>
-                      <option value=""></option>
-                      <option value=""></option>
-                      <option value=""></option>
-                    </select>
-                  </span>
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="field is-horizontal">
-            <div class="field-label is-normal">
-              <label class="label">TE</label>
-            </div>
-            <div class="field-body">
-              <div class="field">
-                <p class="control">
-                  <span class="select">
-                    <select>
-                      <option value=""></option>
-                      <option value=""></option>
-                      <option value=""></option>
-                      <option value=""></option>
-                    </select>
-                  </span>
-                </p>
-              </div>
-            </div>
+          <div class="field">
+            <label class="label">WR</label>
+            <p class="control">
+              <span class="select">
+                <select>
+                  <option></option>
+                  <option></option>
+                </select>
+              </span>
+            </p>
           </div>
         </div>
-        <div class="column has-text-right is-narrow">
-          <div class="field is-horizontal">
-            <div class="field-label is-normal">
-              <label class="label">Flex</label>
-            </div>
-            <div class="field-body">
-              <div class="field">
-                <p class="control">
-                  <span class="select">
-                    <select>
-                      <option value=""></option>
-                      <option value=""></option>
-                      <option value=""></option>
-                      <option value=""></option>
-                    </select>
-                  </span>
-                </p>
-              </div>
-            </div>
+        <div class="column">
+          <div class="field">
+            <label class="label">TE</label>
+            <p class="control">
+              <span class="select">
+                <select>
+                  <option></option>
+                  <option></option>
+                </select>
+              </span>
+            </p>
           </div>
-          <div class="field is-horizontal">
-            <div class="field-label is-normal">
-              <label class="label">D/ST</label>
-            </div>
-            <div class="field-body">
-              <div class="field">
-                <p class="control">
-                  <span class="select">
-                    <select>
-                      <option value=""></option>
-                      <option value=""></option>
-                      <option value=""></option>
-                      <option value=""></option>
-                    </select>
-                  </span>
-                </p>
-              </div>
-            </div>
+          <div class="field">
+            <label class="label">WR</label>
+            <p class="control">
+              <span class="select">
+                <select>
+                  <option></option>
+                  <option></option>
+                </select>
+              </span>
+            </p>
           </div>
-          <div class="field is-horizontal">
-            <div class="field-label is-normal">
-              <label class="label">K</label>
-            </div>
-            <div class="field-body">
-              <div class="field">
-                <p class="control">
-                  <span class="select">
-                    <select>
-                      <option value=""></option>
-                      <option value=""></option>
-                      <option value=""></option>
-                      <option value=""></option>
-                    </select>
-                  </span>
-                </p>
-              </div>
-            </div>
+          <div class="field">
+            <label class="label">WR</label>
+            <p class="control">
+              <span class="select">
+                <select>
+                  <option></option>
+                  <option></option>
+                </select>
+              </span>
+            </p>
           </div>
-          <div class="field is-horizontal">
-            <div class="field-label is-normal">
-              <label class="label">Bench</label>
-            </div>
-            <div class="field-body">
-              <div class="field">
-                <p class="control">
-                  <span class="select">
-                    <select>
-                      <option value=""></option>
-                      <option value=""></option>
-                      <option value=""></option>
-                      <option value=""></option>
-                    </select>
-                  </span>
-                </p>
-              </div>
-            </div>
+        </div>
+        <div class="column">
+          <div class="field">
+            <label class="label">TE</label>
+            <p class="control">
+              <span class="select">
+                <select>
+                  <option></option>
+                  <option></option>
+                </select>
+              </span>
+            </p>
+          </div>
+          <div class="field">
+            <label class="label">WR</label>
+            <p class="control">
+              <span class="select">
+                <select>
+                  <option></option>
+                  <option></option>
+                </select>
+              </span>
+            </p>
+          </div>
+          <div class="field">
+            <label class="label">WR</label>
+            <p class="control">
+              <span class="select">
+                <select>
+                  <option></option>
+                  <option></option>
+                </select>
+              </span>
+            </p>
           </div>
         </div>
       </div>
+    </div>
     </div>
   </temp>
 </template>
